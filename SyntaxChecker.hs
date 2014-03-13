fb589ebe0de677782a4d9a31464103f96fcd5ffe
happlebao <c.x.bao@student.ucc.ie>
2014-03-13 01:54:34 +0000

    now can check code within interpreter && clean

--- SyntaxChecker.hs
+++ SyntaxChecker.hs
@@ -1,6 +1,6 @@
-module SyntaxChecker (isCoreModule, getModule) where
+module SyntaxChecker (isCoreModule, isCoreExpression, getModule, getNames) where
 
-import           Language.Haskell.Exts
+import Language.Haskell.Exts
 
 
 -- Disable module Name, default is Main even if you omit it
@@ -33,14 +33,12 @@ isCoreImportDecl _ = error "You can't use import declaration."
 
 -- Check top-level declaration
 -- FunBind example: f n = n + 1
--- d: the declaration, ns: the name defined in current file
-isCoreDecl :: [Decl] -> Bool
-isCoreDecl ds = all (isCoreDecl' names) ds
-      where
-        names = getNames ds
-        isCoreDecl' ns (PatBind _ p t r b) =
-            isCorePat ns p && isCoreType  t &&
-            isCoreRhs ns r && isCoreBinds b
+-- d: the declaration, ns: the names of top-level definition
+isCoreDecl :: [String] -> [Decl] -> Bool
+isCoreDecl ns = all (isCoreDecl' ns) where
+        isCoreDecl' ns' (PatBind _ p t r b) =
+            isCorePat ns' p && isCoreType  t &&
+            isCoreRhs ns' r && isCoreBinds b
         isCoreDecl' _ _ = error "You can only use pattern binding."
 
 
@@ -62,15 +60,19 @@ isCoreExp :: [String] -> Exp -> Bool
 isCoreExp ns (Var qn) = isCoreQName ns qn
 isCoreExp ns (Con qn) = isCoreQName ns qn
 isCoreExp _ (Lit l) = isCoreLiterial l
-isCoreExp ns (InfixApp e1 qop e2) = isCoreExp ns e1 && isCoreExp ns e2 && isCoreQOp qop
-isCoreExp ns (App e1 e2) = isCoreExp ns e1 && isCoreExp ns e2
+isCoreExp ns (InfixApp expr1 qop expr2) = isCoreExp ns expr1 && isCoreExp ns expr2 && isCoreQOp qop
+isCoreExp ns (App expr1 expr2) = isCoreExp ns expr1 && isCoreExp ns expr2
 isCoreExp ns (NegApp e) = isCoreExp ns e
+isCoreExp ns (If expr1 expr2 e3) = isCoreExp ns expr1 && isCoreExp ns expr2 && isCoreExp ns e3
 -- ns: names, vs: variables
-isCoreExp ns (Lambda _ ps e) = all (isCorePat (ns ++ vs)) ps && isCoreExp (ns ++ vs) e
+isCoreExp ns (Lambda _ ps expr) = all (isCorePat (ns ++ vs)) ps && isCoreExp (ns ++ vs) expr
     where vs = map getNameFromPat ps
-isCoreExp ns (List es) = all (isCoreExp ns) es
+isCoreExp ns (List exprs) = all (isCoreExp ns) exprs
+-- LeftSection: (1+)
+isCoreExp ns (LeftSection expr qop) = isCoreExp ns expr && isCoreQOp qop
+-- RightSection (+1)
+isCoreExp ns (RightSection qop expr) = isCoreExp ns expr && isCoreQOp qop
 isCoreExp ns (Paren e) = isCoreExp ns e
-isCoreExp ns (If e1 e2 e3) = isCoreExp ns e1 && isCoreExp ns e2 && isCoreExp ns e3
 isCoreExp _ _ = error (
     "You can only use lambda expression," ++
     "parenthesis, list, if and operator defined in core-haskell." )
@@ -114,8 +116,8 @@ isCoreName ns (Symbol s) = s `elem`
     ||  error ("You can't use " ++ show s)
 
 -- Get name from top level definitions
-getNames :: [Decl] -> [String]
-getNames = map getName
+getNames :: Module -> [String]
+getNames (Module _ _ _ _ _ _ ds) = map getName ds
         where
         getName (PatBind _ p _ _ _) = getNameFromPat p
         getName _ = error "You can only use pattern binding"
@@ -137,15 +139,17 @@ isCoreLiterial _ = error "You can only use Char String Int"
 
 
 -- Check the whole module/file
-isCoreModule :: Module -> Bool
-isCoreModule (Module _ mn mp wt es im d) =
+isCoreModule :: [String] -> Module -> Bool
+isCoreModule ns (Module _ mn mp wt es im ds) =
     isCoreModuleName  mn && isCoreModulePragma mp &&
     isCoreWarningText wt && isCoreExportSpec   es &&
-    isCoreImportDecl  im && isCoreDecl         d
+    isCoreImportDecl  im && isCoreDecl         ns ds
 
--- Check single statement whithin interpreter
-isCoreStatement :: String -> Bool
-isCoreStatement = undefined
+-- Check single expression whithin interpreter
+-- Can't do any definiton by default: let where a=x <- etc
+isCoreExpression :: [String] -> String -> Bool
+isCoreExpression ns expr = isCoreExp ns (fromParseResult (parseExp expr))
+    -- isCoreDecl [fromParseResult (parseDecl expr)] 
 
 
 getModule :: String -> IO Module
@@ -155,12 +159,12 @@ getModule mPath = do
     return . fromParseResult $ parseModule src
 
  --below just used for test the parse result of haskell-src-ext
-printAlldecl :: Module -> IO()
-printAlldecl (Module _ _ _ _ _ _ ds) = mapM_  print ds
+--printAlldecl :: Module -> IO()
+--printAlldecl (Module _ _ _ _ _ _ ds) = mapM_  print ds
 
-main :: IO ()
-main = do
-  m <- getModule "Hello.hs"
-  print m
+--main :: IO ()
+--main = do
+--  m <- getModule "Hello.hs"
+--  print m
   --printAlldecl m
   --print (isCoreModule m)
